0fed7e0338f4401f887c59fcc1369d3ac244e94a
prasannakumar <prasannakumar.mandapati6303@gmail.com>
2024-02-26 15:25:32 -0600

    changed data type values for testing

--- CastingInJava/ExplicitCasting.java
+++ CastingInJava/ExplicitCasting.java
@@ -11,12 +11,12 @@ public class ExplicitCasting {
          * Here in explicit casting we are storing big datatypes
          * of requirements into small datatypes.
          */
-        double d = 778955.5566;
+        double d = 77955.5566;
         System.out.println("value of d is " + d);
         int i = (int) d;
         System.out.println("value of i is " + i);
 
-        long l = 77899566625L;
+        long l = 7799566625L;
         System.out.println("value of l is " + l);
         int intValue = (int) l;
         System.out.println("vlaue of intValue is " + intValue);
